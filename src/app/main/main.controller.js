7571242e2af78d1ed1a5130bfb0d1e59f948a2d7
scalka <calka.sylwia@gmail.com>
2016-11-10 18:26:04 +0000

    writing to db

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,11 +1,19 @@
 export class MainController {
-  constructor () {
+  constructor ($http) {
     'ngInject';
+
+    this.$http = $http; /*we are using the Angular HTTP service in order to post our data to the following URL. 
+                        So we will need reference to HTTP. Let's inject the HTTP into our constructor and pass its reference to a class property we create. 
+                        And we'll do that with this.$http and we'll send it to the reference we just passed in.
+                        And so now if we copy this, we can replace HTTP with this.$http*/
 
   }
 
   postMessage(){
     console.log("==postMessage");
+    //communicating with server
+    // this.message refers to ng-model="message" in main.html
+    this.$http.post('http://localhost:5000/api/message', {msg: this.message});
   }
 
 }
